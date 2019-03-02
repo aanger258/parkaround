344c15cdaf8cc3c0b2eead8652db8088542f93e6
Cosmin Musteata <cosmin.g.musteata@gmail.com>
2019-03-02 16:49:38 +0200

    added locations database

---
+++ database/migrations/2019_03_02_142139_create_locations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->boolean('status');
+            $table->integer('type');
+            $table->integer('price');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
