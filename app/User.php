ca3ce369a66342b26b628f1532e4c63b91e553ad
Cosmin Musteata <cosmin.g.musteata@gmail.com>
2019-03-02 17:51:44 +0200

    changed json response

--- app/User.php
+++ app/User.php
@@ -47,13 +47,13 @@ class User extends Authenticatable
         $new_user->email = $request->email;
         $new_user->password = Hash::make($request->password);
         if($new_user->save())
-            return json_encode('OK');
-        return json_encode('NOT OK');
+            return true;
+        return false;
     }
 
     public static function checkUser(Request $request){
         if(User::where('email', '=', $request->email)->where('password', '=', $request->password)->count() == 1)
-            return json_encode('OK');
-        return json_encode('NOT OK');
+            return true;
+        return false;
     }
 }
